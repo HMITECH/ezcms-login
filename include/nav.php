6158d694c8dbd0cb38a485329c84d57bcef64fb1
mohdahmed <mo.ahmed@hmi-tech.net>
2019-10-21 23:22:13 +0530

    Neaten up header

--- include/nav.php
+++ include/nav.php
@@ -14,7 +14,7 @@
 	  <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> 
 		<span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> 
 	  </button>
-	  <a class="brand" href="../"><small>ezCMS : </small></a>
+	  <a class="brand" href="../"><small>ezCMS</small></a>
 	  <div class="nav-collapse collapse">
 		<ul class="nav" id="top-bar">
 		  <li class="dropdown">
