7e8d08232f51a497da798668cfd568bad7f812de
Mo <Mo>
2024-08-30 17:06:48 +0530

    Fixed filemanager config file for php8+ version

--- filemanager/config/config.php
+++ filemanager/config/config.php
@@ -17,7 +17,7 @@ if (!$_SESSION['MANAGEFILES'])
 
 mb_internal_encoding('UTF-8');
 mb_http_output('UTF-8');
-mb_http_input('UTF-8');
+mb_http_input();
 mb_language('uni');
 mb_regex_encoding('UTF-8');
 ob_start('mb_output_handler');
